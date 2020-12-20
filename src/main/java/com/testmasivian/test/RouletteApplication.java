d3aa91672fdf1833a7c2a27020d0d83c26b12900
Villate12 <nickbone553@gmail.com>
2020-12-20 14:03:55 -0500

    First Step for Roulette Test

---
+++ src/main/java/com/testmasivian/test/RouletteApplication.java
@@ -0,0 +1,16 @@
+package com.testmasivian.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteApplication.class, args);
+	}
+
+}
